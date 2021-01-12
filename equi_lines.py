d018407eec592b00917fcb0366557b587f514966
pknittel <pknittel@umich.edu>
2021-01-12 14:15:34 -0600

    sort of working

--- equi_lines.py
+++ equi_lines.py
@@ -6,6 +6,8 @@ import numpy as np
 #position of charges
 pos = (-1.001, 0)
 neg = (1.001, 0)
+pos2 = (0, -1.001)
+neg2 = (0, 1.001)
 #magnitude of charge
 Qneg = -1e-9
 Qpos = 1e-9
@@ -16,26 +18,31 @@ min_neg = -2
 max_pos = 2
 
 #electric potential at point (x,y) due to a dipole
-def E(x, y):
+def E2(x, y):
     hyppos = np.hypot(x - pos[0], y - pos[1])
     hypneg = np.hypot(x - neg[0], y - neg[1])
     # E_potential = (k * Q / r)
     return k * Qneg / hypneg + k * Qpos / hyppos
 
+#electric potential at point (x,y) due to 4 charges
+def E4(x, y):
+    hyppos = np.hypot(x - pos[0], y - pos[1])
+    hypneg = np.hypot(x - neg[0], y - neg[1])
+    hyppos2 = np.hypot(x - pos2[0], y - pos2[1])
+    hypneg2 = np.hypot(x - neg2[0], y - neg2[1])
+    # E_potential = (k * Q / r)
+    return k * Qpos / hypneg + k * Qpos / hyppos + \
+            k * Qneg / hypneg2 + k * Qneg / hyppos2 
+
 x = np.linspace(min_neg, max_pos, 1001)
 y = np.linspace(min_neg, max_pos, 1001)
-
 X, Y = np.meshgrid(x, y)
-Z = E(X, Y)
+Z = E4(X, Y)
 
 l = [1.8 ** x for x in range(-3, 9, 1)]
 l = [-x for x in reversed(l)] + [0] + l
-
 plt.contour(X, Y, Z, l, colors='black');
-
 #plt.contour(X, Y, Z, l, cmap='RdGy');
-
 plot = plt.contourf(X, Y, Z, l, cmap='BuPu')
 plt.colorbar();
-
 plt.show()
--- test2.py
+++ test2.py
@@ -8,6 +8,8 @@ from matplotlib.patches import Circle
 #position of charges
 pos = (-1.001, 0)
 neg = (1.001, 0)
+pos2 = (0, -1.001)
+neg2 = (0, 1.001)
 #magnitude of charge
 Qneg = -1e-9
 Qpos = 1e-9
@@ -24,28 +26,6 @@ pos = (-1, 0)
 num_char = 1
 #number of places that E-feild is being calculated at
 num_q = 64 * 64
-
-#electric potential at point (x,y) due to a dipole
-def E(x, y):
-    hyppos = np.hypot(x - pos[0], y - pos[1])
-    hypneg = np.hypot(x - neg[0], y - neg[1])
-    # E_potential = (k * Q / r)
-    return k * Qneg / hypneg + k * Qpos / hyppos
-
-x = np.linspace(min_neg, max_pos, 1001)
-y = np.linspace(min_neg, max_pos, 1001)
-
-X, Y = np.meshgrid(x, y)
-Z = E(X, Y)
-
-l = [1.8 ** x for x in range(-3, 9, 1)]
-l = [-x for x in reversed(l)] + [0] + l
-
-plt.contour(X, Y, Z, l, colors='black');
-
-
-plot = plt.contourf(X, Y, Z, l, cmap='BuPu')
-plt.colorbar();
 
 #Electric field lines
 
@@ -90,8 +70,37 @@ for q, pos in charges:
 
 ax.set_xlabel('x-position')
 ax.set_ylabel('y-position')
-ax.set_title('Electric Field Lines')
+ax.set_title('Electric Field and Equipotential Lines')
 ax.set_xlim(min_neg, max_pos)
 ax.set_ylim(min_neg, max_pos)
 ax.set_aspect('equal')
+
+#electric potential at point (x,y) due to a dipole
+def E2(x, y):
+    hyppos = np.hypot(x - pos[0], y - pos[1])
+    hypneg = np.hypot(x - neg[0], y - neg[1])
+    # E_potential = (k * Q / r)
+    return k * Qneg / hypneg + k * Qpos / hyppos
+
+#electric potential at point (x,y) due to 4 charges
+def E4(x, y):
+    hyppos = np.hypot(x - pos[0], y - pos[1])
+    hypneg = np.hypot(x - neg[0], y - neg[1])
+    hyppos2 = np.hypot(x - pos2[0], y - pos2[1])
+    hypneg2 = np.hypot(x - neg2[0], y - neg2[1])
+    # E_potential = (k * Q / r)
+    return k * Qpos / hypneg + k * Qpos / hyppos + \
+            k * Qneg / hypneg2 + k * Qneg / hyppos2 
+
+x = np.linspace(min_neg, max_pos, 1001)
+y = np.linspace(min_neg, max_pos, 1001)
+X, Y = np.meshgrid(x, y)
+Z = E2(X, Y)
+#logarithmic scaling of colors
+l = [1.8 ** x for x in range(-3, 9, 1)]
+l = [-x for x in reversed(l)] + [0] + l
+plt.contour(X, Y, Z, l, colors='black');
+plot = plt.contourf(X, Y, Z, l, cmap='BuPu')
+plt.colorbar();
+
 plt.show()
